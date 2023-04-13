b359fa091447cfb2c8981da02af4a3c4b75d1124
Mohanekm2001 <124660796+Mohanekm2001@users.noreply.github.com>
2023-04-13 11:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+ 
+	const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+function hasKey(key) {
+  return Object.prototype.hasOwnProperty.call(sampleObject, key);
+}
+
+console.log(hasKey("red")); // true
+console.log(hasKey("blue")); // false
+console.log(hasKey("white")); // true
+
+
 }
 
 // Do not change the code below
